fe618faa439944781aa59ecc3e3f6a6d58b238ae
Dawid <u56128@student.wat.edu.pl>
2025-10-26 18:44:53 +0100

    dd

--- lib/main.dart
+++ lib/main.dart
@@ -63,8 +63,6 @@ class MyApp extends StatelessWidget {
   };
   @override
   Widget build(BuildContext context) {
-
-    print(user["name"]);
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(),
